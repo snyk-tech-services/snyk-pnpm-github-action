a28547b25b27392bcec70ea3567c4f44dcc4177f
Mathilde <mathilde.riou@snyk.io>
2021-08-06 11:39:20 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+![Snyk logo](https://snyk.io/style/asset/logo/snyk-print.svg)
+
+***
+
+[![Known Vulnerabilities](https://snyk.io/test/github/snyk-tech-services/snyk-pnpm-github-action/badge.svg)](https://snyk.io/test/github/snyk-tech-services/snyk-pnpm-github-action)
+
+Snyk helps you find, fix and monitor for known vulnerabilities in your dependencies, both on an ad hoc basis and as part of your CI (Build) system.
+
+## Snyk snyk-pnpm-github-action
+GitHub action converting a pnpm lockfile into a DepGraph and send it to the snyk API to scan
---
+++ src/lib/index.ts
@@ -0,0 +1,65 @@
+const github = require('@actions/github')
+const core = require('@actions/core')
+import * as apiTool from '../../../snyk-pnpm-dephtree-api-tool';
+
+
+const runAction = async () => {
+     
+    const breakBuild: boolean = core.getInput('breakBuild') == 'true' ? true : false
+    try{
+        const snykToken: string = core.getInput('snykToken');
+        const snykOrganization: string = core.getInput('snykOrganization');
+        const path: string = core.getInput('pnpmLockfilePath') == '.' ? '/' : core.getInput('pnpmLockfilePath')
+        const includeDev: string = core.getInput('includeDev');
+
+        checkSnykToken(snykToken)
+
+        process.argv = [
+          '--root',
+          path,
+          '--snykToken',
+          snykToken,
+          '--orgId',
+          snykOrganization,
+          '--includeDev',
+          includeDev,
+        ];
+
+        const result = await apiTool.main();
+        
+    } catch(err) {
+        console.log("Failed Check !")
+        if(breakBuild) {
+            core.setFailed(err)
+        } else {
+            console.log(err)
+        }
+
+    }
+}
+
+const checkSnykToken = (snykToken: string) => {
+  const regex = /[^a-f0-9\-]/
+  if(!isStringAgainstRegexOK(snykToken,regex)){
+      throw new Error("Unauthorized characters in snyk token")
+  }
+  
+}
+
+const isStringAgainstRegexOK = (stringItem: string, regex: RegExp): boolean => {
+  const blacklistedCharacters = stringItem.match(regex)
+  if (blacklistedCharacters && blacklistedCharacters.length > 0) {
+      return false
+  }
+  return true
+}
+
+
+if(!module.parent){
+    runAction()
+}
+ 
+
+export {
+    runAction
+}
